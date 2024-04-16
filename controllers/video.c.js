7929a37001f382c74c8f762bf47822d4423fb7b2
21127154 <henryquoccao@gmail.com>
2024-04-16 11:27:48 +0700

    add: scale poster + update: add video on poster

--- controllers/video.c.js
+++ controllers/video.c.js
@@ -2,11 +2,12 @@ module.exports = {
     watch: async (req, res) => {
         let theme = req.cookies.theme;
         let dark = theme === "dark" ? true : false;
-
+        let title = req.query.name;
         res.render('video', {
             title: 'Movie',
             video: true,
-            dark: dark
+            dark: dark,
+            title
         })
     }
 };
