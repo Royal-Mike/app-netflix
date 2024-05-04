49cb3a26f94d1e4d47c69b2dbc09e5efa6253102
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-05-04 22:39:57 +0700

    display user info

--- controllers/profile.c.js
+++ controllers/profile.c.js
@@ -1,9 +1,11 @@
-// const profileM = require('../models/profile.m');
+const accountM = require('../models/account.m');
 
 module.exports = {
     profile: async (req, res) => {
+        const data = await accountM.getAccount(req.user.username);
         res.render('account/profile', {
             title: 'Profile',
+            data: data,
             home: true,
             profile: true
         })
