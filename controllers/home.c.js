9658ede171ebd977b9e1582889ac17238addb8a1
LDuyTan <leduytan2304@gmail.com>
2024-05-06 18:22:08 +0700

    test
    test

--- controllers/home.c.js
+++ controllers/home.c.js
@@ -7,6 +7,35 @@ const homeM = require('../models/home.m');
 
 module.exports = {
     home: async (req, res) => {
+        try {
+			let theme = req.cookies.theme;
+			let dark = theme === "dark" ? true : false;
+			const user_id = await accountM.getUserIdByUsername(req.user.username);
+			const subscription = await subscribeM.getSubscriptionByUserId(user_id);
+
+			// Check if a user has a subscription or expired subscription
+			if (!subscription) {
+				res.redirect('/subscribe');
+			} else {
+				if (subscription.status === 'none'|| subscription.end_date < new Date())
+				res.redirect('/subscribe');
+				const popular_movies = await homeM.getPopularMovies();
+				const now_playing_movies = await homeM.getNowPlayingMovies();
+				const top_rated_movies = await homeM.getTopRatedMovies();
+				const upcoming_movies = await homeM.getUpcomingMovies();
+				res.render('home', {
+					title: 'Home',
+					home: true,
+					dark: dark,
+					popular_movies,
+					now_playing_movies,
+					top_rated_movies,
+					upcoming_movies
+				});
+			}
+		} catch (error) {
+			console.error('Error getting home page:', error);
+		}
         let theme = req.cookies.theme;
         let userID = req.session.username;
 
