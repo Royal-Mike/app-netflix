68c47d1c080e0efe400b88dd0c375cff4d816675
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-05-06 11:26:18 +0700

    show membership expire date

--- controllers/profile.c.js
+++ controllers/profile.c.js
@@ -3,9 +3,11 @@ const accountM = require('../models/account.m');
 module.exports = {
     profile: async (req, res) => {
         const data = await accountM.getAccount(req.user.username);
+        const sub = await accountM.getSubscription(req.user.id);
         res.render('account/profile', {
             title: 'Profile',
             data: data,
+            sub: sub,
             home: true,
             profile: true
         })
